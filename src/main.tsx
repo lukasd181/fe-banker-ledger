494a1b3b01743a20d1fffde3e1c1e68fbd37b39b
lukasd181 <hungduonggia181@gmail.com>
2025-04-02 16:26:13 -0400

    added google login

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
-createRoot(document.getElementById('root')!).render(
+const clientID: string = import.meta.env.VITE_CLIENT_ID ?? "default_id";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <GoogleOAuthProvider clientId={clientID}>
+      <App />
+    </GoogleOAuthProvider>
+  </StrictMode>
+);
